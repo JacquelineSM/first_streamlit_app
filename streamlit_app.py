baee4b1083e3a703efb8d1c6a0c293886d042f5c
JacquelineSM <149347606+JacquelineSM@users.noreply.github.com>
2023-10-29 21:29:58 -0600

    Update streamlit_app.py
    added breakfast menu header and 3 items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My parents her buy a new car')
+
+streamlit.header('Menú de desayuno')
+streamlit.text('Omega 3 y avena con arándanos')
+streamlit.text('Batido de col rizada, espinacas y rúcula')
+streamlit.text('Huevo duro de gallinas camperas')
